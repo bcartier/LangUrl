c0fd2959719c966cfc3497261940a2324799366d
Rémy <git@rrelmy.ch>
2017-06-26 17:52:22 +0200

    use statamic core methods to generate urls / and fixes multiple bugs

--- LangUrlTags.php
+++ LangUrlTags.php
@@ -2,10 +2,10 @@
 
 namespace Statamic\Addons\LangUrl;
 
+use Statamic\Contracts\Data\Content\Content;
 use Statamic\Extend\Tags;
 use Statamic\API\URL;
 use Statamic\API\Data;
-use Statamic\Data\Services\PagesService;
 
 
 class LangUrlTags extends Tags
@@ -19,48 +19,23 @@ class LangUrlTags extends Tags
     {
         $context = $this->context;
 
+        // target locale
         $locale = $this->getParam('locale', 'default');
         if ($locale == 'default') {
             $locale = default_locale();
         }
 
-        // generate url to frontpage if no page or entry is in the context
-        if (!array_key_exists('id', $context)) {
-            return URL::prependSiteUrl('/', $locale);
+        /** @var Content $content */
+        $content = !empty($context["id"]) ? Data::find($context["id"]) : null;
+        if ($content) {
+            // set the locale on the object
+            $content->locale($locale);
+
+            // return localized url
+            return $content->url();
         }
 
-        $route = "";
-        $slug = "";
-        $content_id = $context["id"];
-        $content_uri = $context["uri"];
-        $contentObject = Data::find($context["id"])->in($locale);
-        $data = $contentObject->get()->dataForLocale($locale);
-        if( array_key_exists('slug',$data)  ){
-          $slug = $data['slug'];
-        }
-
-        if( array_key_exists('is_entry',$context)  ){
-          $collection = $context["collection"];
-          $collectionRoute = $context["settings"]["routes"]["collections"][$collection];
-          if( is_array($collectionRoute) ){
-            $route = $collectionRoute[$locale];
-          } elseif( is_string($collectionRoute) ) {
-            $route = $collectionRoute;
-          }
-          if($slug != ""){
-            $localized_url = str_replace("{slug}",$slug, $route );
-          }
-        }
-
-        if( array_key_exists('is_page',$context)  ){
-
-          $localized_url = app(PagesService::class)
-            ->localizedUris($locale)
-            ->get($content_id, $content_uri );
-        }
-
-        $url = URL::prependSiteUrl($localized_url , $locale);
-
-        return $url ;
+        // generate url to front page if no page or entry is in the context
+        return URL::prependSiteUrl('/', $locale);
     }
 }
